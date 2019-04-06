fae8d0d7dfb41247af564ecaf2fd07e82f9b69a9
Osama Tariq <usama.adil.ghumman@gmail.com>
2019-04-06 19:14:06 +0500

    Arithmatic Operation Ruby

---
+++ Arithmatic.rb
@@ -0,0 +1,37 @@
+x = 123
+
+y = 23
+
+sum = x+y
+
+puts sum
+
+sub = x-y
+
+puts sub
+
+mul = x*y
+
+puts mul
+x = 2.5
+
+y = 3.5
+
+div = x/y
+
+puts div
+
+x = 5 
+
+pow = x **4 #it means 5*5*5*5
+
+puts pow
+
+pow = x **3 #it means 5*5*5
+puts pow
+
+puts "Enter the number "
+
+num = gets
+
+puts num
